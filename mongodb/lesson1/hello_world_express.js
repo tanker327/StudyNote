b36de6e27d6c12c1dd12024eea83846072b0b3e3
Tanker <tanker327@gmail.com>
2014-08-13 16:53:12 -0400

    post version

--- mongodb/lesson1/hello_world_express.js
+++ mongodb/lesson1/hello_world_express.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var cons = require("consolidate");
+var MongoClinet = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+
+var mongoClient = new MongoClinet( new Server('localhost',27017, {'native_parser':true}));
+var db = mongoClient.db('test');
 
 app.engine('html', cons.swig);
 app.set("view engine", "html");
@@ -8,12 +13,18 @@ app.set("views", __dirname+"/views");
 
 
 app.get("/", function (req,res) {
-    res.render("hello", {"name":"swig"});
+    db.collection('hello_mongo_express').findOne({}, function (err, doc) {
+        if(err) throw err;
+        res.render("hello", doc);
+    });
 });
 
 app.get("*", function (req, res) {
     res.send("Page not found.");
 });
 
-app.listen(8080);
-console.log("Server is listening to 8080.");
+mongoClient.open(function(err, mongoclinet){
+    app.listen(8080);
+    console.log("Server is listening to 8080.");
+});
+
