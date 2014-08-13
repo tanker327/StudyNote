b0548a6003755bb830e184f7f8057eee790a52b6
Tanker <tanker327@gmail.com>
2014-08-13 14:29:22 -0400

    Second version

--- mongodb/lesson1/hello_world_express.js
+++ mongodb/lesson1/hello_world_express.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var cons = require("consolidate");
+
+app.engine('html', cons.swig);
+app.set("view engine", "html");
+app.set("views", __dirname+"/views");
 
 
 app.get("/", function (req,res) {
-    res.send("hello world js");
-})
+    res.render("hello", {"name":"swig"});
+});
+
+app.get("*", function (req, res) {
+    res.send("Page not found.");
+});
 
 app.listen(8080);
 console.log("Server is listening to 8080.");
